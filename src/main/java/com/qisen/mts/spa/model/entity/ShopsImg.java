6902a6da08788eb1f529018c04763defe39c6d37
jackhuangjl <874706726@qq.com>
2019-03-01 00:52:15 +0800

    定义商品图片实体

---
+++ src/main/java/com/qisen/mts/spa/model/entity/ShopsImg.java
@@ -0,0 +1,61 @@
+package com.qisen.mts.spa.model.entity;
+
+import com.qisen.mts.common.model.entity.BaseEntity;
+
+
+public class ShopsImg extends BaseEntity{
+   
+	private static final long serialVersionUID = 1L;
+	
+	private Integer id;
+	private Integer eid;
+	private String appId;
+	private Integer goodsId;
+	private String imgUrl;
+	private String type;
+	private String status;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public Integer getEid() {
+		return eid;
+	}
+	public void setEid(Integer eid) {
+		this.eid = eid;
+	}
+	public String getAppId() {
+		return appId;
+	}
+	public void setAppId(String appId) {
+		this.appId = appId;
+	}
+	public Integer getGoodsId() {
+		return goodsId;
+	}
+	public void setGoodsId(Integer goodsId) {
+		this.goodsId = goodsId;
+	}
+	public String getImgUrl() {
+		return imgUrl;
+	}
+	public void setImgUrl(String imgUrl) {
+		this.imgUrl = imgUrl;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+
+}
